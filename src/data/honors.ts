6b404d2f558768eae1c6f4523fe3b28c911aef64
viiccwen <viiccwen@gmail.com>
2026-09-12 01:16:07 +0800

    Remove Coding 101 and ITSA honors

--- src/data/honors.ts
+++ src/data/honors.ts
@@ -16,18 +16,6 @@ export const honors = [
     href: 'https://ithelp.ithome.com.tw/users/20168031/ironman/8221',
   },
   {
-    title: 'Software Engineering Practice Award',
-    subtitle: '2025 Coding 101',
-    period: 'Mar 2025',
-    logo: '/coding101.png',
-  },
-  {
-    title: '5th Place',
-    subtitle: '2023 ITSA National Software Development Contest',
-    period: 'Dec 2023',
-    logo: '/itsa.png',
-  },
-  {
     title: 'Outstanding Youth',
     subtitle: 'National Taiwan University of Science and Technology',
     period: 'Dec 2024',
